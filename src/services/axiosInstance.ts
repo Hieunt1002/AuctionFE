b085dd891e21e1080de83d547eb8c124d3d543bd
Hieunt1002 <nguyenanh0978638@gmail.com>
2024-12-08 23:25:11 +0700

    update

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -2,7 +2,7 @@ import { useMessage } from '@contexts/MessageContext';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5173', // Replace with your API base URL
+  baseURL: 'https://auction-fe-nine.vercel.app', // Replace with your API base URL
 });
 
 // Add a response interceptor to handle both success and error messages
