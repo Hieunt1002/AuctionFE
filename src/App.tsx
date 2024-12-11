a9c1343c2fc6a614c5cff7c889a070fc74bba0a7
Hieunt1002 <nguyenanh0978638@gmail.com>
2024-12-11 22:38:34 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,10 @@ import ListAccountPage from '@pages/Admin/ListAccountPage';
 
 const AppRoutes: React.FC = () => {
   const { isLoading } = useLoading();
+  const getRole = () => {
+    const role = localStorage.getItem('role');
+    return role;
+  };
   return (
     <>
       <MessageModal />
@@ -48,26 +52,33 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/thong-tin-chi-tiet/:id" element={<DetailPage />} />
         <Route path="/" element={<ProtectedRoute />}>
-          <Route path="/phien-dau-gia/:id" element={<DetailPage />} />
-          <Route path="/add-auction" element={<AddActionPage />} />
-          <Route path="/edit-auction" element={<EditActionPage />} />
-          <Route path="/listAuction" element={<ListAuction />} />
-          <Route path="/auctionDetail" element={<AuctionDetail />} />
-          <Route path="/contract" element={<Contract />} />
-          <Route path="/auctionDetail/:id" element={<AuctionDetail />} />
-          <Route path="/listuser" element={<ListAccountPage />} />
+          {getRole() == 'user' && (
+            <>
+              <Route path="/phien-dau-gia/:id" element={<DetailPage />} />
+              <Route path="/add-auction" element={<AddActionPage />} />
+              <Route path="/edit-auction" element={<EditActionPage />} />
+              <Route path="/contract" element={<Contract />} />
+              <Route path="/cancel" element={<CancelPage />} />
+              <Route path="/success" element={<SuccessPage />} />
+              <Route path="/listYourAuction" element={<ListYourAuction />} />
+              <Route path="/detail-auction/:id" element={<DetailAuctionPage />} />
+            </>
+          )}
+          {getRole() == 'admin' && (
+            <>
+              <Route path="/dashboard" element={<Home />} />
+              <Route path="/listAuction" element={<ListAuction />} />
+              <Route path="/addAdminPage" element={<AddAdminPage />} />
+              <Route path="/auctionDetail/:id" element={<AuctionDetail />} />
+              <Route path="/listuser" element={<ListAccountPage />} />
+            </>
+          )}
+          <Route path="/auctionRoom" element={<AuctionRoom />} />
           <Route path="/add-info" element={<AddInfo />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/cancel" element={<CancelPage />} />
-          <Route path="/detail-auction/:id" element={<DetailAuctionPage />} />
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/auctionRoom" element={<AuctionRoom />} />
           <Route path="/changePasswordPage" element={<ChangePasswordPage />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/listYourAuction" element={<ListYourAuction />} />
           <Route path="/inforUser" element={<InforUser />} />
           <Route path="/update-profile" element={<Updateprofile />} />
-          <Route path="/addAdminPage" element={<AddAdminPage />} />
         </Route>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/about" element={<About />} />
